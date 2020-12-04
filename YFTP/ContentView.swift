701702143004ec07b166a81517fa769fcf609310
Tjerk <tjerk@hey.com>
2020-12-05 00:03:13 +0100

    Initial Commit

---
+++ YFTP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YFTP
+//
+//  Created by Tjerk Dijkstra on 05/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YFTP/YFTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YFTPApp.swift
+//  YFTP
+//
+//  Created by Tjerk Dijkstra on 05/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct YFTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
